451c0c64a4bef6c83c5b76770fcea434cb69bb1d
IgorAraujo123 <129716384+IgorAraujo123@users.noreply.github.com>
2026-05-26 14:58:14 -0300

    Update main.jsx

--- src/JSX/main.jsx
+++ src/JSX/main.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Roots from "./roots.jsx";
+import Navbar from "./NavBar";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Navbar />
     <Roots />
   </React.StrictMode>
 );
