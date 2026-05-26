ce8fcc108355a7072b4063dfedcb937a790df3bd
IgorAraujo123 <129716384+IgorAraujo123@users.noreply.github.com>
2026-05-26 14:56:49 -0300

    Update Cadastro.jsx

--- src/JSX/Cadastro.jsx
+++ src/JSX/Cadastro.jsx
@@ -1,122 +1,100 @@
-import { useState } from "react";
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import "/src/CSS/formulario.css"
 
 function App_Cadastro() {
 
-  const [nome, setNome] = useState("");
-  const [senha, setSenha] = useState("");
-  const [senhaConfirm, setsenhaConfirm] = useState("");
-  const [login_status, setLogin_status] = useState(null);
+  const [nome, setNome] = useState("")
+  const [senha, setSenha] = useState("")
+  const [senhaConfirm, setsenhaConfirm] = useState("")
+  const [email, setEmail] = useState("")
+  const [cadastro_status, setCadastro_status] = useState(null)
+  const navigate = useNavigate();
 
-  const registrarNome = (event) => {
-    setNome(event.target.value);
+
+  const registrarNome = (nome) =>{
+    setNome(nome.target.value)
   };
 
-  const registrarSenha = (event) => {
-    setSenha(event.target.value);
+  const registrarSenha = (senha) =>{
+    setSenha(senha.target.value)
   };
 
-  const registrarsenhaConfirm = (event) => {
-    setsenhaConfirm(event.target.value);
+  const registrarsenhaConfirm = (senhaConfirm) =>{
+    setsenhaConfirm(senhaConfirm.target.value)
   };
 
-  const verficacaoCadastro = (resposta) => {
-    if (resposta === "Sucess") {
-      setLogin_status(true);
-    } else {
-      setLogin_status(false);
+  const registrarEmail = (email) =>{
+    setEmail(email.target.value)
+  }
+
+  const verficacaoCadastro = () =>{
+    if(nome === "" || senha === "" || email === "" || senhaConfirm === ""){
+      setCadastro_status("campos_incopletos")
+    }else if(email.endsWith("@gmail.com") === false){
+      setCadastro_status("email_incorreto");
+    }else if(senha != senhaConfirm){
+      setCadastro_status("confirmação_senha_erro")
+    }else{
+      setCadastro_status("cadastro_ok")
+      navigate("/login")
     }
-  };
+     
+  }
 
   return (
+    <>
+    <main className='container'>
 
-    <main
-      className="container d-flex justify-content-center align-items-center vh-100"
-    >
+      <form className='p-4 rounded shadow col-md-6 mx-auto mt-5' id='form_config'>
 
-      <section
-        className="card shadow-lg p-4"
-        style={{
-          width: "450px",
-          borderRadius: "18px"
-        }}
-      >
+        <img src="/images/logo_login/estude.png" height={180} width={180} />
 
-        <h1 className="text-center mb-4">
-          Cadastrar
-        </h1>
+        <div className="mb-3 mt-3 d-grid gap-2">
 
-        <div className="mb-3">
+          <input type="text" className="form-control username_inp back_input" id="exampleFormControlInput1" placeholder="Usuario" value={nome} onChange={registrarNome}></input>
+          
+          <input type="email" className="form-control email_inp back_input" id="exampleFormControlInput1" placeholder="Email" value={email} onChange={registrarEmail}></input>
+          
+          <input type="password" className="form-control senha_inp back_input" id="exampleFormControlInput1" placeholder="Senha" value={senha} onChange={registrarSenha}></input>
+          
+          <input type="password" className="form-control senha_inp back_input" id="exampleFormControlInput1" placeholder="Confirma Senha" value={senhaConfirm} onChange={registrarsenhaConfirm}></input>
+          
+          <p className='d-flex mt-3 gap-2' id='button_group'>
+            
+            <button className="btn btn-primary btn-sm" type="button" onClick={() => verficacaoCadastro()} id='buttons_form'>
+              Cadastrar
+            </button>
 
-          <label className="form-label">
-            Nome
-          </label>
-
-          <input
-            type="text"
-            className="form-control"
-            value={nome}
-            onChange={registrarNome}
-            placeholder="Digite seu Nome"
-          />
-
-        </div>
-
-        <div className="mb-3">
-
-          <label className="form-label">
-            Senha
-          </label>
-
-          <input
-            type="password"
-            className="form-control"
-            value={senha}
-            onChange={registrarSenha}
-            placeholder="Digite sua senha"
-          />
-
-        </div>
-
-        <div className="mb-4">
-
-          <label className="form-label">
-            Confirma Senha
-          </label>
-
-          <input
-            type="password"
-            className="form-control"
-            value={senhaConfirm}
-            onChange={registrarsenhaConfirm}
-            placeholder="Repita a senha"
-          />
-
-        </div>
-
-        <button
-          className="btn btn-primary w-100"
-          onClick={() => verficacaoCadastro(String(""))}
-        >
-          Cadastrar
-        </button>
-
-        {login_status === true && (
-          <p className="text-success text-center mt-3">
+          </p>
+          
+          {cadastro_status == "cadastro_ok" && (
+          <p className="alert alert-success mt-2" role="alert">
             Cadastro realizado com sucesso
-          </p>
-        )}
+          </p>)}
 
-        {login_status === false && (
-          <p className="text-danger text-center mt-3">
-            Cadastro não foi possível
-          </p>
-        )}
+          {cadastro_status == "confirmação_senha_erro" && (
+          <p className="alert alert-danger mt-2" role="alert">
+            Confirmação de senha e senha devem ser iguais
+          </p>)}
 
-      </section>
+          {cadastro_status == "campos_incopletos" && (
+          <p className="alert alert-danger mt-2" role="alert">
+            Complete todos os campos
+          </p>)}
 
+          {cadastro_status == "email_incorreto" && (
+          <p className="alert alert-danger mt-2" role="alert">
+            Coloque @gmail.com no final
+          </p>)}
+
+        </div>
+      </form>
     </main>
-
+    </>
   );
 }
 
-export default App_Cadastro;
+export default App_Cadastro
+
+
