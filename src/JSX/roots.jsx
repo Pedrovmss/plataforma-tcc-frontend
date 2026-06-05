1c1ccf416f43004b34ed17dacb7a9d6fc998191d
IgorAraujo123 <129716384+IgorAraujo123@users.noreply.github.com>
2026-06-05 18:03:28 -0300

    Update roots.jsx

--- src/JSX/roots.jsx
+++ src/JSX/roots.jsx
@@ -3,14 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../App.jsx";
 import App_Login from "./Login";
 import App_Cadastro from "./Cadastro";
+import BuscarProjetos from "./projetos_busca"
+import Erro_body from "./erro_page.jsx";
 
 function Roots() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<BuscarProjetos />} />
+        <Route path="/cadastrar_projetos" element={<App />} />
         <Route path="/login" element={<App_Login />} />
         <Route path="/cadastro" element={<App_Cadastro />} />
+        <Route path="*" element={<Erro_body />}/>
       </Routes>
     </BrowserRouter>
   );
