4c60ac1235246790139598b171e0090885be4d0e
IgorAraujo123 <129716384+IgorAraujo123@users.noreply.github.com>
2026-06-05 17:41:52 -0300

    Update Login.jsx

--- src/JSX/Login.jsx
+++ src/JSX/Login.jsx
@@ -32,7 +32,7 @@ function App_Login() {
       <form className='p-4 rounded shadow col-md-6 mx-auto mt-5'>
 
           <img
-            src="/images/logo_login/estude.png"
+            src="/images/Formularios/estude.png"
             height={180}
             width={180}
             className="d-block mx-auto"
